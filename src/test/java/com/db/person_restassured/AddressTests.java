34c8d43dfdc20184f83a6349cac85ee4f43cd45e
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-04 16:13:00 -0300

    feat: Implement Address Tests

---
+++ src/test/java/com/db/person_restassured/AddressTests.java
@@ -0,0 +1,152 @@
+package com.db.person_restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import com.db.person_restassured.fixture.AddressFixture;
+import com.db.person_restassured.fixture.PersonFixture;
+import com.db.person_restassured.modal.Address;
+import com.db.person_restassured.modal.Person;
+
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
+
+public class AddressTests {
+
+    @Test
+    public void allowAddressCreationAndDeletion() {
+        Person validPerson = PersonFixture.createPersonValid();
+        String personCpf = PersonFixture.createPersonValid().getCpf();
+        Address invalidAddress = AddressFixture.createAddressInvalid();
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validPerson)
+            .when()
+            .post("http://localhost:8080/person/create")
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(invalidAddress)
+            .when()
+            .post("http://localhost:8080/address/create/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value());
+
+        given()
+            .when()
+            .delete("http://localhost:8080/person/delete/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void notAllowAddressCreation() {
+        Person validPerson = PersonFixture.createPersonValid();
+        String personCpf = PersonFixture.createPersonValid().getCpf();
+        Address validAddress = AddressFixture.createAddressValid();
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validPerson)
+            .when()
+            .post("http://localhost:8080/person/create")
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validAddress)
+            .when()
+            .post("http://localhost:8080/address/create/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .when()
+            .delete("http://localhost:8080/person/delete/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void allowListAddress() {
+        Person validPerson = PersonFixture.createPersonValid();
+        String personCpf = PersonFixture.createPersonValid().getCpf();
+        Address validAddress = AddressFixture.createAddressValid();
+        Long addressID = validAddress.getAddressIdentifier();
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validPerson)
+            .when()
+            .post("http://localhost:8080/person/create")
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validAddress)
+            .when()
+            .post("http://localhost:8080/address/create/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validAddress)
+            .when()
+            .patch("http://localhost:8080/address/update/"+addressID)
+            .then()
+            .statusCode(HttpStatus.OK.value());
+
+        given()
+            .when()
+            .get("http://localhost:8080/address")
+            .then()
+            .statusCode(HttpStatus.OK.value());
+
+        given()
+            .when()
+            .delete("http://localhost:8080/person/delete/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void allowAddressUpdate() {
+        Person validPerson = PersonFixture.createPersonValid();
+        String personCpf = PersonFixture.createPersonValid().getCpf();
+        Address validAddress = AddressFixture.createAddressValid();
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validPerson)
+            .when()
+            .post("http://localhost:8080/person/create")
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(validAddress)
+            .when()
+            .post("http://localhost:8080/address/create/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        given()
+            .when()
+            .get("http://localhost:8080/address")
+            .then()
+            .statusCode(HttpStatus.OK.value());
+
+        given()
+            .when()
+            .delete("http://localhost:8080/person/delete/"+personCpf)
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+}
